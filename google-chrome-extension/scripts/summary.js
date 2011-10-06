9ea7317d8ad5e3b8af4e8d77a09059c2839e93eb
yonmanm@gmail.com <yonmanm@gmail.com>
2011-10-06 07:12:35 +0000

    ZSTD-922 Added summary mini-window Added population js function

---
+++ google-chrome-extension/scripts/summary.js
@@ -0,0 +1,8 @@
+function populateSummary(url, requests, events, critical, error, warning) {
+	$('#summary-bar-url').text(url);
+	$('#summary-requests-count').text(requests);
+	$('#summary-events-count').text(events);
+	$('#summary-icon-critical').text(critical);
+	$('#summary-icon-error').text(error);
+	$('#summary-icon-warning').text(warning);
+}
