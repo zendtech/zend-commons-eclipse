c87864e58095ba54461e3ef0cfc689cbb42d4243
amitdarmail@gmail.com <amitdarmail@gmail.com>
2011-10-10 09:13:08 +0000

    ZSTD-955 Change data structure and updated the event switch method

--- google-chrome-extension/scripts/events.js
+++ google-chrome-extension/scripts/events.js
@@ -1,20 +1,20 @@
 function populateEvents(params) {
 	var events = getEvents();
 	populateEventsList(events);
-	switchEvent(events.length - 1);
+	switchEvent(events.length - 1, 0);
 }
 
 function populateEventsList(params) {
 	jQuery.each(params.reverse(), function(index,element) {
-		var event = '<li onClick="switchEvent(' + index + ')" id="event_' + index + '">';
+		var event = '<li id="request_' + index + '">';
 		
 		event += '<div class="event-title">' +
-				 '<div class="studio-icon"></div>' + element.title + '</div><ul>';
+				 '<div class="studio-icon"></div>' + element.url + '</div><ul>';
 		
 		jQuery.each(element.events, function(eventIndex, eventElement) {
 			event += '<li><div class="event-type" id="' + getEventTypeIcon(eventElement.type) + '"></div>' + 
 					 '<div class="event-type" id="debug-small"></div>' +
-					 '<div class="event-type-desc">"' +  eventElement.name + '</div></li>'; 
+					 '<div class="event-type-desc" id="event_' + eventIndex + '" onClick="switchEvent(' + index + ', ' + eventIndex + ')">"' +  eventElement.name + '</div></li>'; 
 		});
 		
 		event += '</ul></li>';
@@ -45,17 +45,20 @@ function getEvents() {
 	return window.events;
 }
 
-function switchEvent(index) {
+function switchEvent(index, eventIndex) {
 	var events = getEvents();
-	event = events[index];
-	
-	console.log($('#event_' + index));
+	event = events[index].events[eventIndex];
 	
 	jQuery.each($('#events-list > li'), function(index, element) {
+		$(element).removeClass('selected-request');
+	});
+	
+	jQuery.each($('.event-type-desc'), function(index, element) {
 		$(element).removeClass('selected-event');
 	});
 	
-	$('#event_' + index).addClass('selected-event');
+	$('#request_' + index).addClass('selected-request');
+	$('#request_' + index + ' #event_' + eventIndex).addClass('selected-event');
 	
 	populateSuperglobals(event.get, event.post, event.cookie, event.server, event.session);
 }
