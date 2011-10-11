f188e58d7c7cabfffcf7920fe1bc0a57425b347a
amitdarmail@gmail.com <amitdarmail@gmail.com>
2011-10-11 16:20:12 +0000

    Cosmetics, fixed center truncation for summary

--- google-chrome-extension/scripts/summary.js
+++ google-chrome-extension/scripts/summary.js
@@ -9,17 +9,19 @@ if (chrome.extension) {
 }
 
 function populateSummary(url, requests, events, critical, warning, normal) {
-	$('#summary-bar-url').text(url);
-	$('#summary-bar-url').truncate({
-	    width: 60,
-	    token: '&hellip;',
-	    center: true,
-	});
 	$('#summary-requests-count').text(requests);
 	$('#summary-events-count').text(events);
 	$('#summary-icon-critical').text(critical);
 	$('#summary-icon-warning').text(warning);
 	$('#summary-icon-normal').text(normal);
+	
+	$('#summary-bar-url').text(url);
+	console.log($('#summary-bar-url'));
+	$('#summary-bar-url').truncate({
+		width: 185,
+		token: '&hellip;',
+		center: true,
+	});
 }
 
 function openTab() {
