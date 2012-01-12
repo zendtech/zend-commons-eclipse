9b2761f62c533c4c39dadd504d065d3188ad3fbc
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2012-01-12 09:13:56 +0000

    Initial commit for org.zend.usagedata.monitors

---
+++ org.zend.usagedata.monitors/src/org/zend/usagedata/internal/monitors/ViewsUsageMonitor.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Zend Technologies Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Zend Technologies Ltd. - initial API and implementation
+ *******************************************************************************/
+package org.zend.usagedata.internal.monitors;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.zend.usagedata.gathering.IUsageDataService;
+import org.zend.usagedata.gathering.IUsageMonitor;
+import org.zend.usagedata.monitors.Activator;
+
+/**
+ * Instances of the {@link ViewsUsageMonitor} class monitor the use of views in
+ * a context of certain perspective. More specifically, it is notified whenever
+ * a view closed or activated (given focus) and sends some of these events to
+ * the UsageDataService.
+ * <p>
+ * List of perspectives which should be consider during collecting information
+ * about views usage is stored in perspectives file.
+ * </p>
+ * 
+ * @author Wojciech Galanciak, 2011
+ * 
+ */
+public class ViewsUsageMonitor implements IUsageMonitor {
+
+	private static final String PERSPECTIVES_FILE = "perspectives"; //$NON-NLS-1$
+	private static final String ACTIVATED = "activated"; //$NON-NLS-1$
+	private static final String VIEW = "view"; //$NON-NLS-1$
+
+	private List<String> perspectives;
+	private IUsageDataService usageDataService;
+
+	private IPartListener partListener = new IPartListener() {
+		public void partActivated(IWorkbenchPart part) {
+			recordEvent(ACTIVATED, part);
+		}
+
+		public void partDeactivated(IWorkbenchPart part) {
+		}
+
+		public void partBroughtToTop(IWorkbenchPart part) {
+		}
+
+		public void partClosed(IWorkbenchPart part) {
+		}
+
+		public void partOpened(IWorkbenchPart part) {
+		}
+	};
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.epp.usagedata.internal.gathering.UsageMonitor#register(org
+	 * .eclipse.epp.usagedata.internal.gathering.UsageDataService)
+	 */
+	public void startMonitoring(IUsageDataService usageDataService) {
+		this.usageDataService = usageDataService;
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		perspectives = getPerspectives();
+		if (perspectives != null && perspectives.size() > 0) {
+			hookListeners(workbench);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.epp.usagedata.internal.gathering.UsageMonitor#deregister()
+	 */
+	public void stopMonitoring() {
+		if (perspectives != null && perspectives.size() > 0) {
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			unhookListeners(workbench);
+		}
+	}
+
+	private void hookListeners(final IWorkbench workbench) {
+		for (IWorkbenchWindow window : workbench.getWorkbenchWindows()) {
+			if (window == null) {
+				return;
+			}
+			for (IWorkbenchPage page : window.getPages()) {
+				page.addPartListener(partListener);
+			}
+		}
+	}
+
+	private void unhookListeners(final IWorkbench workbench) {
+		if (workbench.getDisplay().isDisposed()) {
+			return;
+		}
+		for (IWorkbenchWindow window : workbench.getWorkbenchWindows()) {
+			if (window == null) {
+				return;
+			}
+			for (IWorkbenchPage page : window.getPages()) {
+				page.removePartListener(partListener);
+			}
+		}
+	}
+
+	private void recordEvent(String event, IWorkbenchPart part) {
+		IWorkbenchPartSite site = part.getSite();
+		if (site instanceof IViewSite) {
+			IPerspectiveDescriptor perspective = site.getPage()
+					.getPerspective();
+			if (perspectives.contains(perspective.getId())) {
+				usageDataService.recordEvent(event, VIEW, site.getId(),
+						perspective.getId());
+			}
+		}
+	}
+
+	private List<String> getPerspectives() {
+		List<String> result = new ArrayList<String>();
+		InputStream stream = Activator.getDefault()
+				.getStream(PERSPECTIVES_FILE);
+		if (stream != null) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					stream));
+			String line = null;
+			try {
+				while ((line = reader.readLine()) != null) {
+					result.add(line.trim());
+				}
+			} catch (IOException e) {
+				Activator.log(e);
+				return null;
+			}
+		}
+		return result;
+	}
+
+}
---
+++ org.zend.usagedata.monitors/src/org/zend/usagedata/monitors/Activator.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Zend Technologies Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Zend Technologies Ltd. - initial API and implementation
+ *******************************************************************************/
+package org.zend.usagedata.monitors;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.zend.usagedata.monitors"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	private BundleContext context;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		this.context = context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public static void log(Throwable e) {
+		getDefault().getLog().log(
+				new Status(IStatus.ERROR, PLUGIN_ID, e.getMessage(), e));
+	}
+
+	public InputStream getStream(String name) {
+		InputStream result = null;
+		Bundle bundle = context.getBundle();
+		try {
+			result = bundle.getEntry(name).openStream();
+		} catch (IOException e) {
+			Activator.log(e);
+		}
+		return result;
+	}
+
+}
